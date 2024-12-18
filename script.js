d9f6bb700212d0770e137e7b44f4603141390235
kad5 <khaled.mohamed.1807@gmail.com>
2024-12-18 20:39:30 +0200

    finished dom integration, game history remains

--- script.js
+++ script.js
@@ -1,86 +1,5 @@
 let games = [];
 
-function startNewGame(name1, marker, name2 = "computer") {
-  const game = gamePrototype(name1, marker, name2);
-  let turn = appLogic.checkStartingTurn(game.player1.marker);
-  appLogic.playGame(turn, game.table, game.winner, name2);
-  appLogic.announceWinner(game.winner, game.player1.name, game.player2.name);
-  games.push(game);
-  console.log(games);
-}
-
-function fetchMove() {
-  let recentMove = null;
-  const gridInput = document.querySelector(".app-container");
-  gridInput.addEventListener("click", (e) => {
-    recentMove = e.target.dataset.value;
-  });
-  if (recentMove) return recentMove;
-}
-
-const appLogic = (() => {
-  const checkStartingTurn = function (marker) {
-    if (marker === "x") {
-      return "player1";
-    } else return "player2";
-  };
-  const playGame = function (turn, table, winner, opponent) {
-    while (table.includes(null) && winner[0] === 0) {
-      if (turn === "player1") {
-        let move = prompt("choose from 0 to 8");
-        while (table[move] || isNaN(move) || move < 0 || move > 8) {
-          move = prompt("choose a valid spot");
-        }
-        table[move] = turn;
-        appLogic.checkWinner(table, turn, winner);
-        turn = "player2";
-      } else {
-        if (opponent === "computer") {
-          let move = Math.floor(Math.random() * 9);
-          while (table[move]) {
-            move = Math.floor(Math.random() * 9);
-          }
-          table[move] = turn;
-          appLogic.checkWinner(table, turn, winner);
-          turn = "player1";
-        } else {
-          let move = prompt("choose from 0 to 8");
-          while (table[move] || isNaN(move) || move < 0 || move > 8) {
-            move = prompt("choose a valid spot");
-          }
-          table[move] = turn;
-          appLogic.checkWinner(table, turn, winner);
-          turn = "player1";
-        }
-      }
-    }
-  };
-  const checkWinner = function (table, player, winner) {
-    if (
-      (table[0] === table[1] && table[1] === table[2] && table[2] === player) ||
-      (table[3] === table[4] && table[4] === table[5] && table[5] === player) ||
-      (table[6] === table[7] && table[7] === table[8] && table[8] === player) ||
-      (table[0] === table[3] && table[3] === table[6] && table[6] === player) ||
-      (table[1] === table[4] && table[4] === table[7] && table[7] === player) ||
-      (table[2] === table[5] && table[5] === table[8] && table[8] === player) ||
-      (table[0] === table[4] && table[4] === table[8] && table[8] === player) ||
-      (table[2] === table[4] && table[4] === table[6] && table[6] === player)
-    ) {
-      return (winner[0] = player);
-    }
-  };
-  const announceWinner = function (winner, player1, player2) {
-    if (winner[0] === "player1") {
-      winner[0] = player1;
-      console.log(`${winner[0]} has won the game!`);
-    } else if (winner[0] === "player2") {
-      winner[0] = player2;
-      console.log(`${winner[0]} has won the game!`);
-    } else console.log("it is a draw");
-  };
-  return { checkStartingTurn, playGame, checkWinner, announceWinner };
-})();
-
 const gamePrototype = function (name1, sign, name2) {
   const player1 = { name: name1, marker: sign };
   const player2 = { name: name2, marker: sign === "o" ? "x" : "o" };
@@ -125,6 +44,139 @@ const domVariables = (() => {
     nameDisplay1,
     nameDisplay2,
   };
+})();
+
+const resetBoard = (() => {
+  domVariables.announcer.textContent = `Tic Tac Toe!`;
+  domVariables.nameDisplay1.textContent = `Player1`;
+  domVariables.nameDisplay2.textContent = `Player2`;
+  const list = document.querySelectorAll(".js-choice");
+  list.forEach((e) => {
+    e.textContent = "1";
+  });
+})();
+
+async function startNewGame(name1, marker, name2 = "computer") {
+  const game = gamePrototype(name1, marker, name2);
+  let turn = appLogic.checkStartingTurn(game.player1.marker);
+  await appLogic.playGame(
+    turn,
+    game.table,
+    game.winner,
+    name2,
+    game.player1,
+    game.player2
+  );
+  appLogic.announceWinner(game.winner, game.player1.name, game.player2.name);
+  games.push(game);
+}
+
+const appLogic = (() => {
+  const checkStartingTurn = function (marker) {
+    if (marker === "x") {
+      return "player1";
+    } else return "player2";
+  };
+
+  const playGame = async function (turn, table, winner, opponent, p1, p2) {
+    while (table.includes(null) && winner[0] === 0) {
+      if (turn === "player1") {
+        let move = await awaitMove();
+        while (table[move] || isNaN(move)) {
+          move = await awaitMove();
+        }
+        updateGame(move, turn, table, p1, p2, winner);
+        turn = "player2";
+        updateDisplayMsg(turn, p1, p2);
+      } else {
+        if (opponent === "computer") {
+          let move = Math.floor(Math.random() * 9);
+          while (table[move]) {
+            move = Math.floor(Math.random() * 9);
+          }
+          updateGame(move, turn, table, p1, p2, winner);
+          turn = "player1";
+          updateDisplayMsg(turn, p1, p2);
+        } else {
+          let move = await awaitMove();
+          while (table[move] || isNaN(move)) {
+            move = await awaitMove();
+          }
+          updateGame(move, turn, table, p1, p2, winner);
+          turn = "player1";
+          updateDisplayMsg(turn, p1, p2);
+        }
+      }
+    }
+  };
+
+  function awaitMove() {
+    return new Promise((resolve) => {
+      const gridInput = document.querySelector(".app-container");
+      const handleClick = (e) => {
+        const moveValue = e.target.getAttribute("data-value");
+        if (moveValue !== null) {
+          const move = moveValue;
+          gridInput.removeEventListener("click", handleClick);
+          resolve(move);
+        }
+      };
+      gridInput.addEventListener("click", handleClick);
+    });
+  }
+
+  function updateGame(move, turn, table, p1, p2, winner) {
+    table[move] = turn;
+    updateBoard(table, p1, p2);
+    checkWinner(table, turn, winner);
+  }
+
+  function updateBoard(table, p1, p2) {
+    const list = document.querySelectorAll(".js-choice");
+    list.forEach((e) => {
+      for (let i = 0; i < table.length; i++) {
+        if (i === Number(e.getAttribute("data-value"))) {
+          if (table[i] === "player1") e.textContent = p1.marker;
+          else if (table[i] === "player2") e.textContent = p2.marker;
+          else e.textContent = "";
+        }
+      }
+    });
+  }
+
+  function updateDisplayMsg(turn, p1, p2) {
+    if (turn === "player1")
+      domVariables.announcer.textContent = `${p1.name} turn to play`;
+    else if (turn === "player2")
+      domVariables.announcer.textContent = `${p2.name} turn to play`;
+    else e.textContent = "";
+  }
+
+  const checkWinner = function (table, player, winner) {
+    if (
+      (table[0] === table[1] && table[1] === table[2] && table[2] === player) ||
+      (table[3] === table[4] && table[4] === table[5] && table[5] === player) ||
+      (table[6] === table[7] && table[7] === table[8] && table[8] === player) ||
+      (table[0] === table[3] && table[3] === table[6] && table[6] === player) ||
+      (table[1] === table[4] && table[4] === table[7] && table[7] === player) ||
+      (table[2] === table[5] && table[5] === table[8] && table[8] === player) ||
+      (table[0] === table[4] && table[4] === table[8] && table[8] === player) ||
+      (table[2] === table[4] && table[4] === table[6] && table[6] === player)
+    ) {
+      return (winner[0] = player);
+    }
+  };
+
+  const announceWinner = function (winner, player1, player2) {
+    if (winner[0] === "player1") {
+      winner[0] = player1;
+      domVariables.announcer.textContent = `${winner[0]} has won the game!`;
+    } else if (winner[0] === "player2") {
+      winner[0] = player2;
+      domVariables.announcer.textContent = `${winner[0]} has won the game!`;
+    } else domVariables.announcer.textContent = `It is a draw!`;
+  };
+  return { checkStartingTurn, playGame, announceWinner };
 })();
 
 const eventListeners = (() => {
